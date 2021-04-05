d88bb6ffb8f558422362c306a30771b451a3a97f
staszko <30053337+staszko@users.noreply.github.com>
2021-04-05 13:53:35 +0200

    (J03) In progress

--- src/test/java/com/nurkiewicz/java8/J03_FunctionTest.java
+++ src/test/java/com/nurkiewicz/java8/J03_FunctionTest.java
@@ -5,11 +5,7 @@ import org.junit.Test;
 import org.mockito.InOrder;
 
 import java.util.Date;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.LongConsumer;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.fest.assertions.data.Offset.offset;
@@ -26,99 +22,99 @@ import static org.mockito.Mockito.mock;
 @Ignore
 public class J03_FunctionTest {
 
-	@Test
-	public void shouldPrependHello() {
-		final Function<Integer, String> fun = null;
+    @Test
+    public void shouldPrependHello() {
+        final Function<Integer, String> fun = i -> "Answer is " + i;
 
-		assertThat(fun.apply(42)).isEqualTo("Answer is 42");
-	}
+        assertThat(fun.apply(42)).isEqualTo("Answer is 42");
+    }
 
-	@Test
-	public void shouldProduceAnswer() {
-		final Supplier<Integer> answerFun = null;
+    @Test
+    public void shouldProduceAnswer() {
+        final Supplier<Integer> answerFun = () -> 42;
 
-		assertThat(answerFun.get()).isEqualTo(42);
-	}
+        assertThat(answerFun.get()).isEqualTo(42);
+    }
 
-	@Test
-	public void shouldDecideIfNegative() {
-		final Predicate<Double> isNegative = null;
+    @Test
+    public void shouldDecideIfNegative() {
+        final Predicate<Double> isNegative = t -> Double.valueOf(0d).compareTo(t) < 0;
 
-		assertThat(isNegative.test(3.0)).isFalse();
-		assertThat(isNegative.test(0.0)).isFalse();
-		assertThat(isNegative.test(-1.1)).isTrue();
-	}
+        assertThat(isNegative.test(3.0)).isFalse();
+        assertThat(isNegative.test(0.0)).isFalse();
+        assertThat(isNegative.test(-1.1)).isTrue();
+    }
 
-	@Test
-	public void shouldCallOtherClassInConsumer() {
-		final Date dateMock = mock(Date.class);
+    @Test
+    public void shouldCallOtherClassInConsumer() {
+        final Date dateMock = mock(Date.class);
 
-		final Consumer<Long> consumer = null;
+        final Consumer<Long> consumer = null;
 
-		consumer.accept(1000L);
-		consumer.accept(2000L);
+        consumer.accept(1000L);
+        consumer.accept(2000L);
 
-		final InOrder order = inOrder(dateMock);
-		order.verify(dateMock).setTime(1000L);
-		order.verify(dateMock).setTime(2000L);
-	}
+        final InOrder order = inOrder(dateMock);
+        order.verify(dateMock).setTime(1000L);
+        order.verify(dateMock).setTime(2000L);
+    }
 
-	@Test
-	public void shouldCallOtherClassInPrimitiveConsumer() {
-		final Date dateMock = mock(Date.class);
+    @Test
+    public void shouldCallOtherClassInPrimitiveConsumer() {
+        final Date dateMock = mock(Date.class);
 
-		final LongConsumer consumer = null;
+        final LongConsumer consumer = null;
 
-		consumer.accept(1000L);
-		consumer.accept(2000L);
+        consumer.accept(1000L);
+        consumer.accept(2000L);
 
-		final InOrder order = inOrder(dateMock);
-		order.verify(dateMock).setTime(1000L);
-		order.verify(dateMock).setTime(2000L);
-	}
+        final InOrder order = inOrder(dateMock);
+        order.verify(dateMock).setTime(1000L);
+        order.verify(dateMock).setTime(2000L);
+    }
 
-	@Test
-	public void shouldInvokeReturnedLambdas() throws Exception {
-		//given
-		final Function<String, Integer> strLenFun = createStringLenFunction();
-		final Function<Integer, Double> tripleFun = multiplyFun(3.0);
-		final String input = "abcd";
+    @Test
+    public void shouldInvokeReturnedLambdas() throws Exception {
+        //given
+        final Function<String, Integer> strLenFun = createStringLenFunction();
+        final Function<Integer, Double> tripleFun = multiplyFun(3.0);
+        final String input = "abcd";
 
-		//when
-		final int strLen = strLenFun.apply(input);
-		final double tripled = tripleFun.apply(4);
+        //when
+        final int strLen = strLenFun.apply(input);
+        final double tripled = tripleFun.apply(4);
 
-		//then
-		assertThat(strLen).isEqualTo(4);
-		assertThat(tripled).isEqualTo(4 * 3.0, offset(0.01));
-	}
+        //then
+        assertThat(strLen).isEqualTo(4);
+        assertThat(tripled).isEqualTo(4 * 3.0, offset(0.01));
+    }
 
-	@Test
-	public void shouldComposeFunctionsInVariousWays() throws Exception {
-		//given
-		final Function<String, Integer> strLenFun = createStringLenFunction();
-		final Function<Integer, Double> tripleFun = multiplyFun(3.0);
-		final Function<String, Double> andThenFun = strLenFun.andThen(tripleFun);
-		final Function<String, Double> composeFun = tripleFun.compose(strLenFun);
-		final String input = "abcd";
+    @Test
+    public void shouldComposeFunctionsInVariousWays() throws Exception {
+        //given
+        final Function<String, Integer> strLenFun = createStringLenFunction();
+        final Function<Integer, Double> tripleFun = multiplyFun(3.0);
+        final Function<String, Double> andThenFun = strLenFun.andThen(tripleFun);
+        final Function<String, Double> composeFun = tripleFun.compose(strLenFun);
+        final String input = "abcd";
 
-		//when
-		final double naiveResult = tripleFun.apply(strLenFun.apply(input));
-		final double andThenResult = andThenFun.apply(input);
-		final double composeResult = composeFun.apply(input);
+        //when
+        final double naiveResult = tripleFun.apply(strLenFun.apply(input));
+        final double andThenResult = andThenFun.apply(input);
+        final double composeResult = composeFun.apply(input);
 
-		//then
-		assertThat(naiveResult).isEqualTo(4 * 3.0, offset(0.01));
-		assertThat(andThenResult).isEqualTo(4 * 3.0, offset(0.01));
-		assertThat(composeResult).isEqualTo(4 * 3.0, offset(0.01));
-	}
+        //then
+        assertThat(naiveResult).isEqualTo(4 * 3.0, offset(0.01));
+        assertThat(andThenResult).isEqualTo(4 * 3.0, offset(0.01));
+        assertThat(composeResult).isEqualTo(4 * 3.0, offset(0.01));
+    }
 
-	private Function<Integer, Double> multiplyFun(double times) {
-		throw new UnsupportedOperationException("multiplyFun()");
-	}
+    private Function<Integer, Double> multiplyFun(double times) {
+        throw new UnsupportedOperationException("multiplyFun()");
+    }
 
-	private Function<String, Integer> createStringLenFunction() {
-		throw new UnsupportedOperationException("createStringLenFunction()");
-	}
+    private Function<String, Integer> createStringLenFunction() {
+        throw new UnsupportedOperationException("createStringLenFunction()");
+    }
 
 }
